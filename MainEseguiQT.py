867987e155e6853827c1ac5af4fbd59cff9710bc
fgherardi <fabio.gherardi@cms.it>
2021-04-14 11:03:46 +0200

    Versione con prima interfaccia grafica Qt ( dlgmain )

---
+++ MainEseguiQT.py
@@ -0,0 +1,30 @@
+
+from PyQt5.QtWidgets import QDialog, QApplication
+from dlgmain import Ui_Dialog
+
+
+class finestra(QDialog):
+
+    def __init__(self):
+        super().__init__()
+        self.ui = Ui_Dialog()
+
+        #self.ui.pushButton_click(pushButton_click)
+
+        self.ui.setupUi(self)
+        self.show()
+
+
+    # Codice scritto a mano ( nome evento creato da qt Designer visibile nel file esempio1.py)
+    def PulsantePremuto(self) :
+        print('CIoa pulsante')
+
+   
+
+if __name__ == '__main__' :
+
+	app=QApplication([])
+	appwindow=finestra()
+	appwindow.show()
+	app.exec_()
+   
